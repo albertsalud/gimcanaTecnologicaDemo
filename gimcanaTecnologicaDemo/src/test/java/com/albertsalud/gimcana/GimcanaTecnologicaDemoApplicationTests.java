c6a1007af5cc215f7df41db88dc6488b6a598e46
albert.salud <albert.salud@CVT1191.concatel.com>
2025-01-20 15:58:41 +0100

    initial import

---
+++ gimcanaTecnologicaDemo/src/test/java/com/albertsalud/gimcana/GimcanaTecnologicaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertsalud.gimcana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimcanaTecnologicaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
